ac59a13864ff3e3cbb70bb4a098ea7f77633e545
coding-tsk <thisunk.23078@obhs.school.nz>
2025-08-18 12:06:53 +1200

    Pasted test code from classroom

---
+++ test.py
@@ -0,0 +1,25 @@
+import random
+import string
+
+def display_names(names):
+    for i in names:
+        print(i, end=' ')
+
+def add_ages(ages):
+    total_ages = 0
+    for i in ages:
+        total_ages += i
+    return total_ages
+
+names = []
+ages = []
+num = 0
+
+while(num <= 10):
+    name = input('Enter your name: ')
+    age = input('Enter your age')
+    names.append(name)
+    ages.append(age)
+
+display_names(names)
+add_ages(ages)
